917e73b25886ebf7af59ab827f55219debcea14c
askat123 <asylbekuuluaskat11@gmail.com>
2023-12-12 12:16:59 +0600

    asd

--- src/components/HomePage/partners/index.tsx
+++ src/components/HomePage/partners/index.tsx
@@ -7,6 +7,7 @@ import partner5 from "../../../img/partner5.png";
 import partner6 from "../../../img/partner6.png";
 import partner7 from "../../../img/partner7.jpg";
 import partner8 from "../../../img/partner8.jpg";
+import partner8 from "../../../img/partner9.jpg";
 
 const Partners = () => {
   return (
@@ -63,6 +64,11 @@ const Partners = () => {
               <img
                 style={{ width: "20%", padding: "20px 0" }}
                 src={partner8}
+                alt=""
+              />
+              <img
+                style={{ width: "20%", padding: "20px 0" }}
+                src={partner9}
                 alt=""
               />
             </div>
